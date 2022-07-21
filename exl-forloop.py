6904a75489ac0c788aef2b41744cfa26062f7d21
oktouheed <oktouheed@gmail.com>
2022-07-21 07:57:33 +0600

    Updated ex1 file 2nd line with tteam1 on Github

--- exl-forloop.py
+++ exl-forloop.py
@@ -1,3 +1,4 @@
 print("First modification for 2nd commit")
 print("Added this line of code from zot developers from git terminal")
 print("Added this line from tteam1 branch on Github")
+print("Added 2nd line from tteam1 branch on Github")
